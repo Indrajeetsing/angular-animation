78ef6bbb9e0c75ca8bac1ddcbb8ac5e8800986bf
imandloi <indrajeet925@gmail.com>
2020-08-13 16:42:20 -0400

    added basic rotation animation with rotation speed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    // Each unique animation requires its own trigger. The first argument of the trigger function is the name
+    trigger('rotatedState', [
+      transition('void => *', [
+        animate('{{rotationSpeed}}', style({
+          transform: 'rotate(360deg)'
+        }))
+      ]),
+      transition('* => roatated', [
+        animate('{{rotationSpeed}}', style({
+          transform: 'rotate(360deg)'
+        }))
+      ])
+    ])
+  ]
 })
 export class AppComponent {
-  title = 'angular-animation';
+  state: string = 'void';
+  rotationSpeed: string = '2000ms';
+
+  onAnimationDone(event: AnimationEvent) {
+    // if (event['fromState'] !== 'void') {
+    // setTimeout(() => {
+    this.state = this.state === 'void' ? 'rotated' : 'void';
+    // }, 0);
+    // }
+  }
 }
