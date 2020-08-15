d819d02e86510fcefb29964a0a71c209bf8b566d
imandloi <indrajeet925@gmail.com>
2020-08-15 00:01:41 -0400

    WIP create basic planet rotation with hardcoded objects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { trigger, state, style, animate, transition } from '@angular/animations';
+import { trigger, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   animations: [
-    // Each unique animation requires its own trigger. The first argument of the trigger function is the name
     trigger('rotatedState', [
       transition('void => *', [
         animate('{{rotationSpeed}}', style({
@@ -21,15 +20,30 @@ import { trigger, state, style, animate, transition } from '@angular/animations'
     ])
   ]
 })
-export class AppComponent {
-  state: string = 'void';
-  rotationSpeed: string = '2000ms';
 
-  onAnimationDone(event: AnimationEvent) {
-    // if (event['fromState'] !== 'void') {
-    // setTimeout(() => {
-    this.state = this.state === 'void' ? 'rotated' : 'void';
-    // }, 0);
-    // }
+export class AppComponent implements OnInit {
+
+  public planets: { name: string; color: string; radius: string; state: string; orbitState: string; orbitSpeed: string; spinSpeed: string }[] = [
+    { name: 'Earth', color: 'cornflowerblue', radius: '200px', state: 'void', orbitState: 'void', orbitSpeed: '10000ms', spinSpeed: '2000ms' }
+  ];
+
+  constructor() { }
+
+  ngOnInit() { }
+
+  public onAnimationDone(event: AnimationEvent, index: number, state: string) {
+    this.planets[index][state] = this.planets[index][state] === 'void' ? 'rotated' : 'void';
+  }
+
+  public orbitRadius(ballRadius): object {
+    const numRadius = parseInt(ballRadius, 10);
+    const radiusStyles = {
+      'height': numRadius * 2 + 'px',
+      'margin-top': -numRadius + 'px',
+      'margin-left': -numRadius + 'px',
+      'width': numRadius * 2 + 'px'
+    };
+
+    return radiusStyles;
   }
 }
